73de177d4ff34afd623c69afc1b742aad9bfd434
Jennifer Prince <jennifergraceprince@gmail.com>
2020-08-30 04:25:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do
     "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Jennifer"
+  end
+  
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Temptation by New Order"
+  end
 end
